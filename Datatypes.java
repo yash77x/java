73fac938babbee40d698d2430fd7b796167215d7
yash77x <yash.xworkz@gmail.com>
2023-06-20 22:53:53 +0530

    test push

---
+++ Datatypes.java
@@ -0,0 +1,138 @@
+class Datatypes{
+public static void main(String args[])
+{
+byte age = 44;
+byte totalVowels = 5;
+byte totalAlphabets = 26;
+byte minMarksToPass = 35;
+byte votingAge = 18;
+byte colorInRainbow = 7;
+byte numberOfDaysInWeek = 7;
+byte sideOfTriangle = 3;
+byte iplTrophyWonByMumbai = 5;
+byte ageOfNarendraModi=72;
+
+
+short costOfFan = 3000;
+short totalMarks = 625;
+short pgRent = 10000;
+short costOfGoldPerGram = 5900;
+short numberOfDaysInYear = 365;
+short totalSixesInIpl2023 = 1100;
+short glassSpongeshaslive=10000;
+short distanceFromBangaloreToManali=2678;
+short costOfPetrol10Litre=12000;
+short speedOfAeroplane=575;
+
+int salary = 1200000;
+int populationOfIndia = 1440000000;
+int speedOfLight = 300000;
+int distanceFromEarthToMoon = 384000;
+int speedSoloarRadiation=300000;
+int primeMinisterSalaryINIndiaPerMonth=1000000;
+
+
+
+long worldPopulation = 7880000000L;
+long costlyCarInTheWorld=1900000000L;
+long mobileNumber=918123467578L;
+long bankAccountNumber=765467890234L;
+long telePhoneNumber=786576543425L;
+long atmNumber=67542568976543L;
+
+
+
+ 
+float earthGravity = 9.8F;
+float cgpa = 7.63F;
+
+
+char gender = 'M';
+char tempDegree = 'F';
+char grade = 'A';
+char divisonOfClass='C';
+
+
+boolean isClassOver = true;
+boolean isItSummer = false;
+boolean isJavaUnderstandable = true;
+boolean isFishTasty = false;
+boolean isSundayHoliday = true;
+boolean is3PrimeNumber =true;
+boolean is31EvenNumber =false;
+boolean isJavaplatFormIndpendent=true;
+
+
+
+
+
+
+
+double squareRootOfAreaIs=19.436126731084D;
+double piValue=3.14159265359D;
+double massOfElectron=9.1093837D;
+
+System.out.println(age);
+System.out.println(totalVowels);
+System.out.println(totalAlphabets);
+System.out.println(minMarksToPass);
+System.out.println(votingAge);
+System.out.println(colorInRainbow);
+System.out.println(numberOfDaysInWeek);
+System.out.println(sideOfTriangle);
+System.out.println(iplTrophyWonByMumbai);
+System.out.println(ageOfNarendraModi);
+System.out.println(costOfFan);
+System.out.println(totalMarks);
+System.out.println(pgRent);
+System.out.println(costOfGoldPerGram);
+System.out.println(numberOfDaysInYear);
+System.out.println(totalSixesInIpl2023);
+System.out.println(glassSpongeshaslive);
+System.out.println(distanceFromBangaloreToManali);
+System.out.println(costOfPetrol10Litre);
+System.out.println(speedOfAeroplane);
+System.out.println(salary);
+System.out.println(populationOfIndia);
+System.out.println(speedOfLight);
+System.out.println(distanceFromEarthToMoon);
+System.out.println(speedSoloarRadiation);
+System.out.println(primeMinisterSalaryINIndiaPerMonth);
+System.out.println(worldPopulation);
+System.out.println(costlyCarInTheWorld);
+System.out.println(mobileNumber);
+System.out.println(bankAccountNumber);
+System.out.println(telePhoneNumber);
+System.out.println(atmNumber);
+System.out.println(earthGravity);
+System.out.println(cgpa);
+System.out.println(gender);
+System.out.println(tempDegree);
+System.out.println(grade);
+System.out.println(divisonOfClass);
+System.out.println(isClassOver);
+System.out.println(isItSummer);
+System.out.println(isJavaUnderstandable);
+System.out.println(isFishTasty);
+System.out.println(isSundayHoliday);
+System.out.println(is3PrimeNumber);
+System.out.println(is31EvenNumber);
+System.out.println(isJavaplatFormIndpendent);
+System.out.println(squareRootOfAreaIs);
+System.out.println(piValue);
+System.out.println(massOfElectron);
+//System.out.println(age);
+//System.out.println(age);
+
+
+
+
+
+
+
+
+
+
+
+}
+}
